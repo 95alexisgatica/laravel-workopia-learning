9ca721ae83938b9cc8f5465a350be6f75f25b923
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-01-25 21:15:32 -0300

    18.REQUEST OBJECT AND QUERY PARAMS
    Aprendí a obtener información desde la URL usando métodos del objeto Request (method, url, path, fullUrl, ip, userAgent, header)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/jobs',function() {
+    return 'available Jobs';
+});
+
+//Route::get('/post/{id}', function(string $id) {
+//    return '<h2 style="color:blue"> Post ' . $id . '</h2>';
+//});
+
+// 18.REQUEST OBJECT AND QUERY PARAMS
+Route::get('/test',function(Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
+
+Route::get('/users', function(Request $request) {
+    //return $request->query('name');
+    return $request->query('name');
 });
