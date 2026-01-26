0600820ac886d1185dfab924e7d83a362c2ffedf
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-01-25 21:50:08 -0300

    19.RESPONSE HELPER
    Aprendí a modificar encabezados con el Helper Response, devolver arrays, descargar archivos, guardar y recuperar cookies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,24 +12,33 @@ Route::get('/jobs',function() {
     return 'available Jobs';
 });
 
-//Route::get('/post/{id}', function(string $id) {
-//    return '<h2 style="color:blue"> Post ' . $id . '</h2>';
-//});
-
-// 18.REQUEST OBJECT AND QUERY PARAMS
-Route::get('/test',function(Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header(),
-    ];
+Route::get('/test', function() {
+    return response('Hello Word', 200);
 });
 
-Route::get('/users', function(Request $request) {
-    //return $request->query('name');
-    return $request->query('name');
+// Route::get('/notfound', function() {
+//     return response('Page not found', 404);
+// });
+
+Route::get('/notfound', function() {
+    // puedo modificar el header 
+    // return new Response('Page not found', 404)->header('Content-Type', 'test/html');
+});
+
+Route::get('/test', function() {
+    //puedo devolver arrays
+    // return response()->json(['name' => 'alexio']);
+
+    //agregar datos a las cookies
+    return response()->json(['name' => 'alexio'])->cookie('name', 'Alexio Atun');
+});
+
+Route::get('/read-cookie', function(Request $request) {
+    $cookieValue = $request->cookie('name');
+    return response()->json(['cookie' => $cookieValue]);
+});
+
+Route::get('/download', function() {
+    //puedo devolver arrays
+    return response()->download(public_path('favicon.ico'));
 });
