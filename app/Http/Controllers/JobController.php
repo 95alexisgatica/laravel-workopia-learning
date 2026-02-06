66482afec1d3db9c616a907218de3d33edc6cef5
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-02-05 21:21:03 -0300

    Generate Resource Routes & Methods
    Cree un controller desde resouce y se carga todos los metodos predeterminados, y en Web.php los llamo por la ruta sin nombrarlos uno por uno

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,36 +4,68 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// se extiene el root controller para poder crear metodos
 class JobController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $title = 'Availabel Jobs';
         $jobs = [
-            'web development',
-            'database admin',
-            'systems analyst'
+            'Wep Developer',
+            'Database Admin',
+            'Software Engineer',
+            'System Analyst'
         ];
 
-        return view('jobs.index', compact('title', 'jobs'));
+        return view('jobs.index', compact('jobs'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('jobs.create');
     }
 
-    public function show(string $id)
-    {
-        return "showing job $id";
-    }
-
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
-        $title = $request->input('title');
-        $description = $request->input('description');
+        //
+    }
 
-        return "Title: $title, Description: $description";
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,13 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{id}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
+//SHOW mostrar un recurso puntual
+//Route::get('/jobs', [JobController::class, 'index']);
+//CREATE un formulario para crear un nuevo post
+//Route::get('/jobs/create', [JobController::class, 'create']);
+//SHOW mostrar un recurso puntual generalente id+get
+//Route::get('/jobs/{id}', [JobController::class, 'show']);
+//STORE donde enviamos el form y generalmente sera POST
+//Route::post('/jobs', [JobController::class, 'store']);
+
+Route::resource('jobs', JobController::class);
