0a2c4e6c17d0c3bf85002487ed7a85cf195fbcf4
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-02-11 18:19:51 -0300

    33. y 34. Intro to components, Layouts and slots
    aprendi la forma de crear layouts y plantillas para generar codigo DRY

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>jobs</title>
-</head>
-
-<body>
+<x-layout>
     <h1>Available Jobs</h1>
     <ul>
         @forelse ($jobs as $job)
@@ -16,6 +7,4 @@
             <p>No hay trabajos disponible</p>
         @endforelse
     </ul>
-</body>
-
-</html>
+</x-layout>
