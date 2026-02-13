6604a70187e5dbfe2ae0194cf2e29167d29255ac
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-02-13 11:49:39 -0300

    36.Tailwind CSS & Vite Hot Reloading
    agregue font asome y la carpeta theme_files con estilos rpedeterminados, tambien aguegue a estos el url Helper

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{ $title ?? 'Welcome To Workcopia' }}</title>
 </head>
 
 <body class="bg-gray-100">
     <x-header />
-    <h1>Layout Component</h1>
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
