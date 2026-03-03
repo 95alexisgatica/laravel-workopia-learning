04fef62c760f75fdaa2cda5ee6e79a3e4eee57ca
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-03-03 19:03:10 -0300

    Funciones basicas para crear, traer y mostrar solicitudes

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,8 +3,18 @@
     <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="title">
-        <input type="text" name="description" placeholder="description">
+        <div>
+            <input type="text" name="title" placeholder="title">
+            @error('title')
+                <div class="text-red-500 mt-2 trxt-sm">{{ $message }}</div>
+            @enderror
+        </div>
+        <div>
+            <input type="text" name="description" placeholder="description">
+            @error('description')
+                <div class="text-red-500 mt-2 trxt-sm">{{ $message }}</div>
+            @enderror
+        </div>
         <button type="submit">Submit</button>
     </form>
 </x-layout>
