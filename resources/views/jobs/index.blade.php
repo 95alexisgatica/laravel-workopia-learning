922b3d9ac056e5ce16f19a6052ba8c18748b1feb
alexis.gatica <alexis.gatica@outsourcearg.com>
2026-06-16 18:14:28 -0300

    agregado de los logos, se configuro el controller del home para que traiga trabajos, y el index pages y jobs, se modficaron las tarjetas individuales

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-    <h1>Available Jobs</h1>
-    <ul>
+    <h1 class="text-2xl font-bold">All Jobs</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
-            <li><a href="{{ route('jobs.show', $job->id) }}">{{ $job->title }} - {{ $job->description }}</li>
+            <x-job-card :job="$job" />
         @empty
             <p>No hay trabajos disponible</p>
-        @endforelse
-    </ul>
+        @QAZ°
+    </div>
 </x-layout>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
-    <h1>Welcome to Woropia</h1>
-
+    <h1 class="text-3xl font-bold mb-4 border border-gray-300 p-3">Welcome to Woropia</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-6">
+        @forelse ($jobs as $job)
+            <x-job-card :job="$job" />
+        @empty
+            <p>No hay trabajos disponible</p>
+        @endforelse
+    </div>
+    <a href="{{ route('jobs.index') }}" class="block text-xl text-center">
+        <i class="fa fa-arrow-alt-circle-right"></i> View All Jobs
+    </a>
     <x-bottom-banner />
 </x-layout>
